78a7f36aeca6835702d1ecbf726ae389bfa25d14
Karl Brightman <karl@karlbright.org>
2018-12-22 14:51:27 +0800

    Ensure tweets are randomised correctly

--- Makefile
+++ Makefile
@@ -7,4 +7,4 @@ build: setup cleanc; go build -o target/ffcorpus cmd/ffcorpus/*.go && make copy
 clean: ; rm -f target/ffcorpus
 lambda: setup cleanl; GOOS=linux go build -o target/skippalenik cmd/skippalenik/main.go && make copy
 cleanl: ; rm -f target/skippalenik && rm -f release/lambda.zip
-release: lambda; zip release/skippalenik.zip target/skippalenik target/sentences.txt
+release: lambda; zip -jJ release/skippalenik.zip target/skippalenik target/sentences.txt
--- cmd/skippalenik/main.go
+++ cmd/skippalenik/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dghubble/go-twitter/twitter"
@@ -14,6 +16,8 @@ func Handler() (string, error) {
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
 	client := twitter.NewClient(config.Client(oauth1.NoContext, token))
+
+	rand.Seed(time.Now().UnixNano())
 
 	pick, err := forensicfilescorpus.PickFromFile("sentences.txt", 0, 280)
 	if err != nil {
